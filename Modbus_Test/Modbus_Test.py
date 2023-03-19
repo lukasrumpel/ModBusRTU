8cf5da15e3f7ba421495a49a05c935fac74f1596
lukasrumpel <lukasrumpel@hotmail.com>
2023-03-19 16:14:07 +0100

    Update Modbus_Test.py
    added CRC check

--- Modbus_Test/Modbus_Test.py
+++ Modbus_Test/Modbus_Test.py
@@ -1,6 +1,7 @@
 import serial;
 from tkinter import *
 from threading import*
+import crc
 
 fenster = Tk();
 fenster.title("Modbus Testprogramm") 
@@ -23,6 +24,18 @@ MBnum1.set("00");
 MBnum2.set("01");
 data = [];
 
+def crc16(data : bytearray, offset , length):
+    if data is None or offset < 0 or offset > len(data)- 1 and offset+length > len(data):
+        return 0
+    crc = 0xFFFF
+    for i in range(0, length):
+        crc ^= data[offset + i] << 8
+        for j in range(0,8):
+            if (crc & 0x8000) > 0:
+                crc =(crc << 1) ^ 0x1021
+            else:
+                crc = crc << 1
+    return crc & 0xFFFF
 
 def sendCMD():
     labelSlaveAdr.config(text="");
@@ -30,17 +43,22 @@ def sendCMD():
     labelNumOfBytes.config(text="");
     textData.delete("1.0" , "end")
     cmd = [int(MBadr.get(), base=16), int(MBcmd.get(), base=16), int(MBreg1.get(), base=16), int(MBreg2.get(), base=16), int(MBnum1.get(), base=16), int(MBnum2.get(), base=16)];
+    crcSum =crc16(cmd, 0, len(cmd));
+    crcLB = crcSum & 0xFF;
+    crcHB = crcSum>>8 & 0xFF;
+    cmd = [int(MBadr.get(), base=16), int(MBcmd.get(), base=16), int(MBreg1.get(), base=16), int(MBreg2.get(), base=16), int(MBnum1.get(), base=16), int(MBnum2.get(), base=16), int(hex(crcHB), base=16), int(hex(crcLB), base=16)];
     cmd = bytearray(cmd)
     cmd = cmd + b"\n";
     ser.write(cmd);
     adrOfSlave = ser.read();
     funcCode = ser.read();
     numOfBytes = int.from_bytes(ser.read(), 'big');
-    for i in range(numOfBytes-3):
+    for i in range(numOfBytes-2):
         data.append(int.from_bytes(ser.read(), 'big'));
     labelSlaveAdr.config(text="Adr:" + str(adrOfSlave));
     labelFuncCode.config(text="Funccode: " + str(funcCode));
     labelNumOfBytes.config(text="Bytes: " + str(numOfBytes));
+    data.pop();
     textData.insert(END, data);
     data.clear();
     
